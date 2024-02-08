3c0cad105cb759f668771f423498a5f13151f02e
Peter Randazzo <pete@randazzo.ca>
2024-02-08 00:55:16 -0500

    initial personalization

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,5 @@
 import { defineConfig } from "astro/config";
-import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  integrations: [sitemap()],
-  site: "https://astro-theme-creek.netlify.app/",
+
 });
